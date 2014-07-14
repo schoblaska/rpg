93dafc543aaaffb85a63ffc900787808eeb3b46d
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-07-13 21:41:05 -0500

    throttle movement and allow multiple directions at once

--- lib/pc.rb
+++ lib/pc.rb
@@ -1,10 +1,11 @@
 class RPG::PC
-  attr_accessor :rpg, :x, :y
+  attr_accessor :rpg, :x, :y, :moved
 
   def initialize(rpg)
     @rpg = rpg
     @x = rpg.cols / 2
     @y = rpg.rows / 2
+    @moved = Time.now
   end
 
   def draw
@@ -12,14 +13,26 @@ class RPG::PC
   end
 
   def update
-    if @rpg.window.button_down?(Gosu::Button::KbLeft)
-      @x -= 1
-    elsif @rpg.window.button_down?(Gosu::Button::KbRight)
-      @x += 1
-    elsif @rpg.window.button_down?(Gosu::Button::KbUp)
-      @y -= 1
-    elsif @rpg.window.button_down?(Gosu::Button::KbDown)
-      @y += 1
+    if can_move?
+      move(:left)  if @rpg.window.button_down?(Gosu::Button::KbLeft)
+      move(:right) if @rpg.window.button_down?(Gosu::Button::KbRight)
+      move(:up)    if @rpg.window.button_down?(Gosu::Button::KbUp)
+      move(:down)  if @rpg.window.button_down?(Gosu::Button::KbDown)
     end
+  end
+
+  def can_move?
+    (Time.now.to_f - @moved.to_f) * 1000.0 > 50
+  end
+
+  def move(direction)
+    case direction
+    when :left  then @x -= 1
+    when :right then @x += 1
+    when :up    then @y -= 1
+    when :down  then @y += 1
+    end
+
+    @moved = Time.now
   end
 end
